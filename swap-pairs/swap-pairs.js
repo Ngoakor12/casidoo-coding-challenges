357e491c118f1a60d0c2b2ede5c607db7287aa95
Ngoakor12 <ngoakor12@gmail.com>
2022-08-08 09:11:48 +0200

    solve swapPairs

---
+++ swap-pairs/swap-pairs.js
@@ -0,0 +1,16 @@
+function swapPairs(arr) {
+  if (arr.length > 1) {
+    for (let i = 0; i < arr.length; i += 2) {
+      const temp = arr[i];
+      arr[i] = arr[i + 1];
+      arr[i + 1] = temp;
+    }
+    return arr;
+  }
+  return arr;
+}
+
+console.log(swapPairs([1, 2, 3, 4]));
+console.log(swapPairs([]));
+console.log(swapPairs([1, 5]));
+console.log(swapPairs([3]));
